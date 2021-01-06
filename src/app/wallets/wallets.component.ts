0fd0f70a0f76bbaf875cd586ca3b2aaf1943ce07
Zoë <zohaack@edu.aau.at>
2021-01-06 11:53:31 +0100

    form integration

--- src/app/wallets/wallets.component.ts
+++ src/app/wallets/wallets.component.ts
@@ -1,44 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 import { ApiService } from '../services/api.service';
 import { AuthService } from '../services/auth.service';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-wallets',
   templateUrl: './wallets.component.html',
-  styleUrls: ['./wallets.component.css']
+  styleUrls: ['./wallets.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class WalletsComponent implements OnInit {
 
-  closeResult = '';
+  closeResult: string;
+  walletData: any;
+
 
   constructor(public api: ApiService, public router: Router, public auth: AuthService, private modalService: NgbModal) {
-    
+    //@Zoë fix this to only access the data when valid user id
     if(this.auth.loggedIn()) {
       this.router.navigate(['/wallets'])
     }
+    this.walletData = new FormGroup({
+      walletName: new FormControl(''),
+      walletDesc: new FormControl('')
+    })
+
    }
    
 
   ngOnInit(): void {
     
   }
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
+  openBackDropCustomClass(content) {
+    this.modalService.open(content, {backdropClass: 'light-blue-backdrop'});
   }
 }
